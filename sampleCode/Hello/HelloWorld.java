5d6aab8122cd3edabeb3e01de6c8e4cbc2b55183
alejandro-r-ephrata-teals <alejandro-r-ephrata-teals@outlook.com>
2020-09-14 16:02:48 +0000

    HelloWorld.java

--- sampleCode/Hello/HelloWorld.java
+++ sampleCode/Hello/HelloWorld.java
@@ -2,6 +2,6 @@ package sampleCode.Hello;
 
 public class HelloWorld {
     public static void main (String [] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello world");
     }
 }
