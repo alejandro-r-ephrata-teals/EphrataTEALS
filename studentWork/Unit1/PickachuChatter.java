2c07802d7ac1bcdc4ed3fd5dfc7caca441fc32b8
alejandro-r-ephrata-teals <alejandro-r-ephrata-teals@outlook.com>
2020-09-17 00:34:27 +0000

    Pikachu challenge

---
+++ studentWork/Unit1/PickachuChatter.java
@@ -0,0 +1,18 @@
+package studentWork.Unit1;
+
+public class PickachuChatter {
+    public static void main (String[] args) {
+        System.out.println("Pika pika pika chu pika chu peeeee ka pika chu!");
+        drawPikachu();
+        System.out.println("Pika? Pika pika pika chu peeeee ka chu!");
+        drawPikachu();
+    }
+
+    
+    public static void drawPikachu() {
+        System.out.println("(\\__/)");
+        System.out.println("(o^.^)");
+        System.out.println("z(_(\")(\")");
+
+    }
+}
