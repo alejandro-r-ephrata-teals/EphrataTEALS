dd40437209382b82495c70e9019f1f382c90cd67
alejandro-r-ephrata-teals <alejandro-r-ephrata-teals@outlook.com>
2020-09-23 16:06:43 +0000

    Code Errors

---
+++ studentWork/CodeErrors/HelloEphrata.java
@@ -0,0 +1,8 @@
+package studentWork.CodeErrors;
+
+public class HelloEphrata {
+
+    public static void main (String [] args) {
+        System.out.println("Hello Ephrata");
+    }
+}
---
+++ studentWork/CodeErrors/SecretMessage.java
@@ -0,0 +1,10 @@
+package studentWork.CodeErrors;
+
+public class SecretMessage {
+
+    public static void main(String[] args) {
+        System.out.println("Speak friend");
+        System.out.println("and enter");
+        
+    }
+}
