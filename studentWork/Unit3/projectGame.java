3e2cb6c9261bc6600f6f3d4e74bd8868b2e172b6
alejandro-r-ephrata-teals <alejandro-r-ephrata-teals@outlook.com>
2020-12-02 21:42:47 +0000

    ProjectGame.java

--- studentWork/Unit3/projectGame.java
+++ studentWork/Unit3/projectGame.java
@@ -1,3 +1,4 @@
+
 package studentWork.Unit3;
 
 import  java.util.Scanner;
@@ -7,6 +8,9 @@ public class projectGame {
     public static void main(String args[]) {
         
         Scanner console = new Scanner(System.in);
+
+        intro1();
+        intro2();
 
         System.out.print("Hello, Player 1 what is your name? ");
         String player1 = console.nextLine();
@@ -24,6 +28,7 @@ public class projectGame {
     
 
         while (points1 < 10 || points2 < 10){
+
         System.out.print( player1 + " please type \"!roll\" ");    //Must set a another variable == to the players rolls, then check every time to see if the previous roll was the same
         String roll1 = console.nextLine();
         player1Roll = (int)(Math.random() * 6) + 1; 
@@ -37,8 +42,8 @@ public class projectGame {
         System.out.println(player2 + " rolled " + player2Roll);
         System.out.println();
 
-
-        
+        char initialsOfPlayer1 = player1.charAt(0);
+        char initialsOfPlayer2 = player2.charAt(0);
 
         if (roll1.equals("!roll") & roll2.equals("!roll")){
 
@@ -47,39 +52,39 @@ public class projectGame {
             if (player1Roll > player2Roll){
 
                 points1 += 1;
-
-                System.out.println(player1 + " Wins this round with a roll of " + player1Roll );
+                System.out.println(player1 + " Wins this round" );
                 System.out.println();
-                System.out.println(player1 + " Now has " + points1 + " point(s)");
-                System.out.println();
-
-                
+                System.out.println("  " + initialsOfPlayer1 + "   " + initialsOfPlayer2);
+                System.out.println("---------");
+                System.out.println("| " + points1 + " | " + points2 + " |" );
+                System.out.println("---------");
 
             } else if(player2Roll > player1Roll) {
 
                 points2 += 1;
 
-                System.out.println(player2 + " Wins this round with a roll of " + player2Roll );
+                System.out.println(player1 + " Wins this round" );
                 System.out.println();
-                System.out.println(player2 + " Now has " + points2 + " point(s)");
-                System.out.println();
+                System.out.println("  " + initialsOfPlayer1 + "   " + initialsOfPlayer2);
+                System.out.println("---------");
+                System.out.println("| " + points1 + " | " + points2 + " |" );
+                System.out.println("---------");
 
             } else if (player1Roll == player2Roll) {
 
-                points1 = (int)(Math.random() * 2) + 1 + points1;
-                points2 = (int)(Math.random() * 2) + 1 + points2;
+                points1 = (int)(Math.random() * 2) + 1;
+                points2 = (int)(Math.random() *2) + 1;
 
                 System.out.println("You both tied!");
                 System.out.println();
-                System.out.println(player1 + " Has " + points1 + " Point(s)");
+                System.out.println(player1 + " Wins this round" );
                 System.out.println();
-                System.out.println(player2 + " Has " + points2 + " Point(s)");
-                
-
+                System.out.println("  " + initialsOfPlayer1 + "   " + initialsOfPlayer2);
+                System.out.println("---------");
+                System.out.println("| " + points1 + " | " + points2 + " |" );
+                System.out.println("---------");
 
             }
-
-            
 
             if (player1Roll == checkSameRoll1 ){
 
@@ -97,38 +102,81 @@ public class projectGame {
             checkSameRoll2 = player2Roll;
 
 
-
         } else {
             System.out.println("You typed the wrong word, Try again");
         }
-    } if (points1 > points2) {
+    }
 
-        System.out.println(player1 + " Wins with a total of " + points1);
+        if (points1 > points2) {
 
-    } else if (points2 > points1) {
+            System.out.println(player1 + " Wins with a total of " + points1);
+            System.out.println();
+            System.out.println("█   █  ███  █   █ █   █ █████ █   █ ");
+            System.out.println(" █ █  █   █ █   █ █   █   █   ██  █ ");
+            System.out.println("  █   █   █ █   █ █ █ █   █   █ █ █ ");
+            System.out.println("  █   █   █ █   █ ██ ██   █   █  ██ ");
+            System.out.println("  █    ███  █████ █   █ █████ █   █ ");
 
-        System.out.println(player2 + " Wins with a total of " + points2);
 
-    }else if (points1 == points2) {
+        } else if (points2 > points1) {
 
-        System.out.println("IT WAS A TIE!");
+            System.out.println(player2 + " Wins with a total of " + points2);
+            System.out.println();
+            System.out.println("█   █  ███  █   █   █   █ █████ █   █ ");
+            System.out.println(" █ █  █   █ █   █   █   █   █   ██  █ ");
+            System.out.println("  █   █   █ █   █   █ █ █   █   █ █ █ ");
+            System.out.println("  █   █   █ █   █   ██ ██   █   █  ██ ");
+            System.out.println("  █    ███  █████   █   █ █████ █   █ ");
+
+        } else if (points1 == points2) {
+
+            System.out.println("IT WAS A TIE!");
+            System.out.println();
+            System.out.println("█████ █████    █   █  ███  █████     ███     █████ █████ █████ ");
+            System.out.println("  █     █      █   █ █   █ █        █   █      █     █   █     ");
+            System.out.println("  █     █      █ █ █ █████ █████    █████      █     █   ████  ");
+            System.out.println("  █     █      ██ ██ █   █     █    █   █      █     █   █     ");
+            System.out.println("█████   █      █   █ █   █ █████    █   █      █   █████ █████ ");
+
+        } 
 
     }
 
+public static void intro2() {
 
-    }
-
-
-public static void intro() {
+    System.out.println("");
+    System.out.println("▀█▀ █▄ █ █▀▀ ▀█▀ █▀█ █ █ █▀▀ ▀█▀ ▀█▀ █▀█ █▄ █ █▀▀");
+    System.out.println("▄█▄ █ ▀█ ▄██  █  █▀▄ █▄█ █▄▄  █  ▄█▄ █▄█ █ ▀█ ▄██ ");
+    System.out.println("");
     System.out.println("Hello, welcome to, FIRST TO TEN, in this game");
     System.out.println("you will roll a dice by typing in \"!roll\", \nYour opponent will do the same \nWhoever rolls the biggest number receives 1 point ");
     System.out.println("If on the next roll you receive the same roll from the previous time \nYou will receive 2 points no matter what");
     System.out.println("First one to 10 points wins the game and becomes the king of luck :) \nHAVE FUN!");
+    System.out.println();
 
 
+
+}
+public static void intro1() {
+
+    System.out.println("____________________________________________________________________________");
+    System.out.println("                              Welcome to The                                ");
+    System.out.println();
+    System.out.println("             ████  █████  ████ █████     ████  ███  █   █ █████  ");
+    System.out.println("             █   █   █   █     █        █     █   █ ██ ██ █     ");
+    System.out.println("             █   █   █   █     ████     █  ██ █████ █ █ █ ████  ");
+    System.out.println("             █   █   █   █     █        █   █ █   █ █   █ █     ");
+    System.out.println("             ████  █████  ████ █████     ███  █   █ █   █ █████ ");
+    System.out.println("____________________________________________________________________________");
+      
 
 }
 
 
 
 }
+
+
+
+
+
